450906d7513337d665e532824a370db3e6374133
Owen Beaudry <owenbeaudry@gmail.com>
2017-08-07 10:23:27 -0600

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    BeaudryViewTool::Renderer.copyright 'Owen Beaudry', 'All rights reserved'
+  end
 end
